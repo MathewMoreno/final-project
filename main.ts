ff623c411dc6d7ed15214ec54e82c7f8d774294d
MathewMoreno <58610701+MathewMoreno@users.noreply.github.com>
2020-03-03 14:01:26 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,182 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+d d f e e e e e e e e e e f d d 
+d d f f f f f f f f f f f f d d 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+4 f 4 f 4 f 4 f 4 f 4 f 4 f 4 f 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d f f f f f f f f f f f f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e f f e f d d 
+d d f e e e e e e e f f e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+d d f e e e e e e e e e e f d d 
+`
+}
+let Kanye: Sprite = null
+tiles.setTilemap(tiles.createTilemap(
+            hex`0f000800030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030307040404040404040404040404040406020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`,
+            img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.dungeon.darkGroundNorthWest1,myTiles.tile1,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7],
+            TileScale.Sixteen
+        ))
+let Age = 0
+if (info.score() == 0) {
+    Kanye = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . e f f f 
+. . . 1 1 1 e e e e e e e e f f 
+. . 1 1 1 1 e e e e e e e e e f 
+. . 1 e 1 1 e e e e e e e e e f 
+. . . e . . . . . . e . e f e f 
+. e e e . . . . . . e e . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+    scene.cameraFollowSprite(Kanye)
+    info.changeScoreBy(1)
+} else if (Age == 8) {
+	
+} else {
+	
+}
+controller.moveSprite(Kanye)
+game.onUpdate(function () {
+	
+})
